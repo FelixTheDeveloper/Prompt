fd49032caf5672ac45d48c1eddeade50d5fdf7cb
ducng99 <tuanduc249.td@gmail.com>
2019-08-20 16:26:31 +1200

    Update Constraint UI

---
+++ app/src/main/java/nz/prompt/FirstTimeActivity.java
@@ -0,0 +1,14 @@
+package nz.prompt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time);
+    }
+}
---
+++ app/src/main/java/nz/prompt/register/RegisterActivity.java
@@ -0,0 +1,4 @@
+package nz.prompt.register;
+
+public class RegisterActivity {
+}
