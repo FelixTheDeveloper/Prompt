0052f5a7020f5a78ad94f01d718303442a965140
ducng99 <tuanduc249.td@gmail.com>
2019-08-20 15:57:36 +1200

    First UI layout

---
+++ app/src/main/java/nz/prompt/database/DatabaseHandler.java
@@ -0,0 +1,5 @@
+package nz.prompt.database;
+
+public class DatabaseHandler {
+    public static boolean loggedIn = false;
+}
---
+++ app/src/main/java/nz/prompt/login/LoginActivity.java
@@ -0,0 +1,4 @@
+package nz.prompt.login;
+
+public class LoginActivity {
+}
