58f345b81b8050713f4daa9e8b109835907ab9d0
ducng99 <tuanduc249.td@gmail.com>
2019-09-13 22:51:29 +1200

    Initial push for DB

--- app/src/main/java/nz/prompt/database/DatabaseHandler.java
+++ app/src/main/java/nz/prompt/database/DatabaseHandler.java
@@ -1,5 +1,28 @@
 package nz.prompt.database;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
     public static boolean loggedIn = true;
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "PromptDatabase.db";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
