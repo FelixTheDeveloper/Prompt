7d8aab4d3c970911b7416bd46de038735d0c2530
FelixTheDeveloper <qtf0423@autuni.ac.nz>
2019-09-16 22:52:58 +1200

    Allows permission

---
+++ app/src/test/java/nz/prompt/controllers/TaskControllerTest.java
@@ -0,0 +1,12 @@
+package nz.prompt.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaskControllerTest {
+
+    @Test
+    public void addTask() {
+    }
+}
