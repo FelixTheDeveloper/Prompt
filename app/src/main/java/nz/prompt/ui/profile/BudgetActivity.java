5e92bf58693a8a85901829755e62cc2d242bff9d
ducng99 <tuanduc249.td@gmail.com>
2019-09-20 20:57:24 +1200

    Updated Profile page and Budget page

--- app/src/main/java/nz/prompt/ui/profile/BudgetActivity.java
+++ app/src/main/java/nz/prompt/ui/profile/BudgetActivity.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import nz.prompt.R;
+import nz.prompt.controllers.UserController;
 
+/**
+ * @author Carlo
+ */
 public class BudgetActivity extends AppCompatActivity {
 
 
     private Button Subtract;
-    private EditText TotalBudget;
+    private TextView TotalBudget;
     private EditText Expense;
     private EditText RemainingBudget;
     private Button button;
@@ -25,37 +30,22 @@ public class BudgetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_budget);
 
         Subtract = findViewById(R.id.budget_substractButton);
-        TotalBudget = findViewById(R.id.budget_totalBudgetTextBox);
+        TotalBudget = findViewById(R.id.budget_budgetInput);
         Expense = findViewById(R.id.budget_expenseTextBox);
         RemainingBudget = findViewById(R.id.budget_remainingBudgetTextBox);
         button = findViewById(R.id.budget_backbutton);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                back();
-            }
-        });
+        button.setOnClickListener(v -> finish());
+
+        TotalBudget.setText(String.valueOf(UserController.currentUser.getBudget()));
 
         //Subtracts the expenses to the budget
-        Subtract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double budget,expense,remaining;
-                budget = Double.parseDouble(TotalBudget.getText().toString());
-                expense = Double.parseDouble(Expense.getText().toString());
-                remaining = budget-expense;
-                RemainingBudget.setText(Double.toString(remaining));
-            }
+        Subtract.setOnClickListener(v -> {
+            double budget,expense,remaining;
+            budget = Double.parseDouble(TotalBudget.getText().toString());
+            expense = Double.parseDouble(Expense.getText().toString());
+            remaining = budget-expense;
+            RemainingBudget.setText(Double.toString(remaining));
         });
     }
-
-    public void back() {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-    }
-
-
-
-
 }
