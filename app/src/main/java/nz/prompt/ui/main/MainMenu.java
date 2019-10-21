46df93c39c1e91e9e1b16177a21691d5adef6b5a
FelixTheDeveloper <qtf0423@autuni.ac.nz>
2019-10-22 00:36:17 +1300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/nz/prompt/ui/main/MainMenu.java

--- app/src/main/java/nz/prompt/ui/main/MainMenu.java
+++ app/src/main/java/nz/prompt/ui/main/MainMenu.java
@@ -19,6 +19,7 @@ import java.util.Date;
 import java.util.HashSet;
 
 import nz.prompt.R;
+import nz.prompt.api.LocationRestaurants;
 import nz.prompt.controllers.TaskController;
 import nz.prompt.controllers.UserController;
 import nz.prompt.model.TaskModel;
@@ -31,6 +32,7 @@ import nz.prompt.ui.tasks.TaskListActivity;
  */
 public class MainMenu extends AppCompatActivity {
     private ImageButton addTaskButton, profileButton;
+    private Button zomatoButton;
     private CalendarView calendarView;
     private LocalDate cacheDate = LocalDate.now();
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
@@ -45,6 +47,9 @@ public class MainMenu extends AppCompatActivity {
 
         addTaskButton = findViewById(R.id.mainMenu_addTaskButton);
         addTaskButton.setOnClickListener(v -> openTaskPage());
+
+        zomatoButton = findViewById(R.id.foodSuggest);
+        zomatoButton.setOnClickListener(view -> openZomato());
 
         profileButton = findViewById(R.id.mainMenu_profileButton);
         profileButton.setOnClickListener(v -> openProfilePage());
@@ -84,6 +89,11 @@ public class MainMenu extends AppCompatActivity {
         tasks.forEach(this::addRow);
 
         super.onResume();
+    }
+
+    public void openZomato() {
+        Intent intent = new Intent(this, LocationRestaurants.class);
+        startActivity(intent);
     }
 
     public void openTaskPage(){
